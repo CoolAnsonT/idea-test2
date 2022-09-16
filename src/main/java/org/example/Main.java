69f00ffc2d24c0b944af376fa579f199a32d7f10
CoolAnsonT <anson.tang.ds@gmail.com>
2022-09-16 16:30:12 -0400

    push test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Hello hot-fix!");
         System.out.println("Hello master1!");
         System.out.println("Hello hot-fix2!");
+        System.out.println("push test!");
     }
 }
