44fb35d15be4728755c7f94bffe0841230f78aa4
CoolAnsonT <anson.tang.ds@gmail.com>
2022-09-16 16:40:56 -0400

    push test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,6 @@ public class Main {
         System.out.println("Hello hot-fix2!");
         System.out.println("push test!");
         System.out.println("pull test! - fix from the browser");
-        System.out.println("conflict test - from browser");
+        System.out.println("conflict test1 - from intellij");
     }
 }
